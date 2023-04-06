33b3b2d661df7d884385dfb0f1a9a6809e310e0a
osparking <jbpark03@gmail.com>
2023-04-06 15:15:59 +0900

    initial without any dependency

---
+++ src/main/java/space/bumtiger/customLogout/CustomLogoutApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.customLogout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLogoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomLogoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumtiger/customLogout/CustomLogoutApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumtiger.customLogout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomLogoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
